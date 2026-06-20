97593b53924e3b5d905db0b639237feb1046e39f
mjgonzo11 <mjgonzo11@gmail.com>
2026-06-19 20:22:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,1 @@
-version: 1
-frontend:
-  phases:
-    build:
-      commands:
-        - printf 'window.QUOTE_API_URL = "%s";\n' "$QUOTE_API_URL" > config.js
-  artifacts:
-    baseDirectory: .
-    files:
-      - "**/*"
-  cache:
-    paths: []
+window.QUOTE_API_URL = "";
